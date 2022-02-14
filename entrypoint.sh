f47a5a5e362edb15cf55872e008f96e034ace59c
sven <sven@so36.net>
2022-02-14 16:30:55 +0100

    chore(test) with input

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,10 +27,10 @@ split_on_commas "$1" | while read item; do
   echo -e "Validating: ${item}"
   jsonlint --quiet --compact "${item}" | tee $ERROR_LOG
   if [ -s "$ERROR_LOG" ]; then
-    echo -e "${RED}Ooh, there was an error${RESET}"
+    #echo -e "${RED}Ooh, there was an error${RESET}"
     echo ::set-output name=exit_code::1
   else
-    echo -e "${GREEN}Great, all good${RESET}"
+    #echo -e "${GREEN}Great, all good${RESET}"
     echo ::set-output name=status::'Success'
   fi
 done
